7650ad4fcb427c9c2262810276c9cdc0d9e8cb6a
Maks <maksym.shershen@gmail.com>
2022-07-27 21:39:02 +0200

    -

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import { Title } from './HomePage.styled';
 export default function HomePage() {
   return (
     <Container>
-      <Title>Hello! This is Phonebook</Title>
+      <Title>Phonebook</Title>
     </Container>
   );
 }
